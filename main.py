49f0cb2278d7ff9cfe5a11621a765f4a58951db6
gareth <anythingwithawire@gmail.com>
2022-01-25 01:00:57 +0800

    tidied up, added sub button, works but not the action

--- main.py
+++ main.py
@@ -11,20 +11,278 @@ from PyQt5.QtWidgets import QGraphicsScene
 from PyQt5.QtWidgets import QGraphicsView
 from PyQt5.QtWidgets import QMainWindow
 
+import sys
+from PyQt5.QtCore import Qt, QRectF, QPointF
+from PyQt5.QtGui import QBrush, QPainterPath, QPainter, QColor, QPen, QPixmap
+from PyQt5.QtWidgets import QGraphicsRectItem, QApplication, QGraphicsView, QGraphicsScene, QGraphicsItem
+
 listNode = []
 numberNode = 0
 temporListNodes = []
 
 
+class GraphicsRectItem(QGraphicsRectItem):
+
+    handleTopLeft = 1
+    handleTopMiddle = 2
+    handleTopRight = 3
+    handleMiddleLeft = 4
+    handleMiddleRight = 5
+    handleBottomLeft = 6
+    handleBottomMiddle = 7
+    handleBottomRight = 8
+
+    handleSize = +8.0
+    handleSpace = -4.0
+
+    handleCursors = {
+        handleTopLeft: Qt.SizeFDiagCursor,
+        handleTopMiddle: Qt.SizeVerCursor,
+        handleTopRight: Qt.SizeBDiagCursor,
+        handleMiddleLeft: Qt.SizeHorCursor,
+        handleMiddleRight: Qt.SizeHorCursor,
+        handleBottomLeft: Qt.SizeBDiagCursor,
+        handleBottomMiddle: Qt.SizeVerCursor,
+        handleBottomRight: Qt.SizeFDiagCursor,
+    }
+
+    def __init__(self, *args):
+        """
+        Initialize the shape.
+        """
+        super().__init__(*args)
+        self.handles = {}
+        self.handleSelected = None
+        self.mousePressPos = None
+        self.mousePressRect = None
+        self.setAcceptHoverEvents(True)
+        self.setFlag(QGraphicsItem.ItemIsMovable, True)
+        self.setFlag(QGraphicsItem.ItemIsSelectable, True)
+        self.setFlag(QGraphicsItem.ItemSendsGeometryChanges, True)
+        self.setFlag(QGraphicsItem.ItemIsFocusable, True)
+        self.updateHandlesPos()
+
+    def handleAt(self, point):
+        """
+        Returns the resize handle below the given point.
+        """
+        for k, v, in self.handles.items():
+            if v.contains(point):
+                return k
+        return None
+
+    def hoverMoveEvent(self, moveEvent):
+        """
+        Executed when the mouse moves over the shape (NOT PRESSED).
+        """
+        if self.isSelected():
+            handle = self.handleAt(moveEvent.pos())
+            cursor = Qt.ArrowCursor if handle is None else self.handleCursors[handle]
+            self.setCursor(cursor)
+        super().hoverMoveEvent(moveEvent)
+
+    def hoverLeaveEvent(self, moveEvent):
+        """
+        Executed when the mouse leaves the shape (NOT PRESSED).
+        """
+        self.setCursor(Qt.ArrowCursor)
+        super().hoverLeaveEvent(moveEvent)
+
+    def mousePressEvent(self, mouseEvent):
+        """
+        Executed when the mouse is pressed on the item.
+        """
+        self.handleSelected = self.handleAt(mouseEvent.pos())
+        if self.handleSelected:
+            self.mousePressPos = mouseEvent.pos()
+            self.mousePressRect = self.boundingRect()
+        super().mousePressEvent(mouseEvent)
+
+    def mouseMoveEvent(self, mouseEvent):
+        """
+        Executed when the mouse is being moved over the item while being pressed.
+        """
+        if self.handleSelected is not None:
+            self.interactiveResize(mouseEvent.pos())
+        else:
+            super().mouseMoveEvent(mouseEvent)
+
+    def mouseReleaseEvent(self, mouseEvent):
+        """
+        Executed when the mouse is released from the item.
+        """
+        super().mouseReleaseEvent(mouseEvent)
+        self.handleSelected = None
+        self.mousePressPos = None
+        self.mousePressRect = None
+        self.update()
+
+    def boundingRect(self):
+        """
+        Returns the bounding rect of the shape (including the resize handles).
+        """
+        o = self.handleSize + self.handleSpace
+        return self.rect().adjusted(-o, -o, o, o)
+
+    def updateHandlesPos(self):
+        """
+        Update current resize handles according to the shape size and position.
+        """
+        s = self.handleSize
+        b = self.boundingRect()
+        self.handles[self.handleTopLeft] = QRectF(b.left(), b.top(), s, s)
+        self.handles[self.handleTopMiddle] = QRectF(b.center().x() - s / 2, b.top(), s, s)
+        self.handles[self.handleTopRight] = QRectF(b.right() - s, b.top(), s, s)
+        self.handles[self.handleMiddleLeft] = QRectF(b.left(), b.center().y() - s / 2, s, s)
+        self.handles[self.handleMiddleRight] = QRectF(b.right() - s, b.center().y() - s / 2, s, s)
+        self.handles[self.handleBottomLeft] = QRectF(b.left(), b.bottom() - s, s, s)
+        self.handles[self.handleBottomMiddle] = QRectF(b.center().x() - s / 2, b.bottom() - s, s, s)
+        self.handles[self.handleBottomRight] = QRectF(b.right() - s, b.bottom() - s, s, s)
+
+    def interactiveResize(self, mousePos):
+        """
+        Perform shape interactive resize.
+        """
+        offset = self.handleSize + self.handleSpace
+        boundingRect = self.boundingRect()
+        rect = self.rect()
+        diff = QPointF(0, 0)
+
+        self.prepareGeometryChange()
+
+        if self.handleSelected == self.handleTopLeft:
+
+            fromX = self.mousePressRect.left()
+            fromY = self.mousePressRect.top()
+            toX = fromX + mousePos.x() - self.mousePressPos.x()
+            toY = fromY + mousePos.y() - self.mousePressPos.y()
+            diff.setX(toX - fromX)
+            diff.setY(toY - fromY)
+            boundingRect.setLeft(toX)
+            boundingRect.setTop(toY)
+            rect.setLeft(boundingRect.left() + offset)
+            rect.setTop(boundingRect.top() + offset)
+            self.setRect(rect)
+
+        elif self.handleSelected == self.handleTopMiddle:
+
+            fromY = self.mousePressRect.top()
+            toY = fromY + mousePos.y() - self.mousePressPos.y()
+            diff.setY(toY - fromY)
+            boundingRect.setTop(toY)
+            rect.setTop(boundingRect.top() + offset)
+            self.setRect(rect)
+
+        elif self.handleSelected == self.handleTopRight:
+
+            fromX = self.mousePressRect.right()
+            fromY = self.mousePressRect.top()
+            toX = fromX + mousePos.x() - self.mousePressPos.x()
+            toY = fromY + mousePos.y() - self.mousePressPos.y()
+            diff.setX(toX - fromX)
+            diff.setY(toY - fromY)
+            boundingRect.setRight(toX)
+            boundingRect.setTop(toY)
+            rect.setRight(boundingRect.right() - offset)
+            rect.setTop(boundingRect.top() + offset)
+            self.setRect(rect)
+
+        elif self.handleSelected == self.handleMiddleLeft:
+
+            fromX = self.mousePressRect.left()
+            toX = fromX + mousePos.x() - self.mousePressPos.x()
+            diff.setX(toX - fromX)
+            boundingRect.setLeft(toX)
+            rect.setLeft(boundingRect.left() + offset)
+            self.setRect(rect)
+
+        elif self.handleSelected == self.handleMiddleRight:
+            print("MR")
+            fromX = self.mousePressRect.right()
+            toX = fromX + mousePos.x() - self.mousePressPos.x()
+            diff.setX(toX - fromX)
+            boundingRect.setRight(toX)
+            rect.setRight(boundingRect.right() - offset)
+            self.setRect(rect)
+
+        elif self.handleSelected == self.handleBottomLeft:
+
+            fromX = self.mousePressRect.left()
+            fromY = self.mousePressRect.bottom()
+            toX = fromX + mousePos.x() - self.mousePressPos.x()
+            toY = fromY + mousePos.y() - self.mousePressPos.y()
+            diff.setX(toX - fromX)
+            diff.setY(toY - fromY)
+            boundingRect.setLeft(toX)
+            boundingRect.setBottom(toY)
+            rect.setLeft(boundingRect.left() + offset)
+            rect.setBottom(boundingRect.bottom() - offset)
+            self.setRect(rect)
+
+        elif self.handleSelected == self.handleBottomMiddle:
+
+            fromY = self.mousePressRect.bottom()
+            toY = fromY + mousePos.y() - self.mousePressPos.y()
+            diff.setY(toY - fromY)
+            boundingRect.setBottom(toY)
+            rect.setBottom(boundingRect.bottom() - offset)
+            self.setRect(rect)
+
+        elif self.handleSelected == self.handleBottomRight:
+
+            fromX = self.mousePressRect.right()
+            fromY = self.mousePressRect.bottom()
+            toX = fromX + mousePos.x() - self.mousePressPos.x()
+            toY = fromY + mousePos.y() - self.mousePressPos.y()
+            diff.setX(toX - fromX)
+            diff.setY(toY - fromY)
+            boundingRect.setRight(toX)
+            boundingRect.setBottom(toY)
+            rect.setRight(boundingRect.right() - offset)
+            rect.setBottom(boundingRect.bottom() - offset)
+            self.setRect(rect)
+
+        self.updateHandlesPos()
+
+    def shape(self):
+        """
+        Returns the shape of this item as a QPainterPath in local coordinates.
+        """
+        path = QPainterPath()
+        path.addRect(self.rect())
+        if self.isSelected():
+            for shape in self.handles.values():
+                path.addEllipse(shape)
+        return path
+
+    def paint(self, painter, option, widget=None):
+        """
+        Paint the node in the graphic view.
+        """
+        painter.setBrush(QBrush(QColor(255, 0, 0, 100)))
+        painter.setPen(QPen(QColor(0, 0, 0), 1.0, Qt.SolidLine))
+        painter.drawRect(self.rect())
+
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setBrush(QBrush(QColor(255, 0, 0, 255)))
+        painter.setPen(QPen(QColor(0, 0, 0, 255), 1.0, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
+        for handle, rect in self.handles.items():
+            if self.handleSelected is None or handle == self.handleSelected:
+                painter.drawEllipse(rect)
+
+
 class WindowClass(QMainWindow):
     def __init__(self, parent=None):
+
         QMainWindow.__init__(self, parent)
         self.view = ViewClass()
         # self.setCentralWidget(self.view)
         self.h = QHBoxLayout(self)
         self.h.addWidget(self.view)
         self.butt = QPushButton("Print")
+        self.sub = QPushButton("Sub")
         self.h.addWidget(self.butt)
+        self.h.addWidget(self.sub)
         self.setLayout(self.h)
 
         self.mainWid = QWidget(self)
@@ -33,6 +291,7 @@ class WindowClass(QMainWindow):
         self.mainWid.setLayout(self.h)
 
         self.butt.clicked.connect(self.printButt)
+        self.sub.clicked.connect(SceneClass.sub)
 
     def printButt(self):
         print("Print listNode")
@@ -81,9 +340,7 @@ class ViewClass(QGraphicsView):
         delta = newPos - oldPos
         self.translate(delta.x(), delta.y())
         return
-        #event.accept()
-        # do not propagate the event to the scroll area scrollbars
-        return True
+
 
 class SceneClass(QGraphicsScene):
     prepStartEndNode = 0
@@ -120,7 +377,11 @@ class SceneClass(QGraphicsScene):
             self.saveNodeToGlobalList()
         # def wheelEvent(self, source, event):
 
-
+    def sub(self):
+        print("sub")
+        sub = GraphicsRectItem(0,0,2100, 1500)
+        sub.setPos(0,0)
+        self.addItem(sub)
 
     def saveNodeToGlobalList(self):
         global listNode, numberNode, temporListNodes
@@ -181,7 +442,6 @@ class SceneClass(QGraphicsScene):
         super(SceneClass, self).mousePressEvent(event)
 
 
-
 class Node(QGraphicsEllipseItem):
     def __init__(self, rect=QRectF(-20, -20, 20, 20), parent=None):
         QGraphicsEllipseItem.__init__(self, rect, parent)
@@ -227,8 +487,7 @@ class Node(QGraphicsEllipseItem):
         return QGraphicsItem.itemChange(self, change, value)
 
 
-
-class XNode(QGraphicsItem):
+class XNode(QGraphicsRectItem):
     """
     A simple QGraphicsItem that can be dragged around the scene.
     Of course, this behavior is easier to achieve if you simply use the default
@@ -251,6 +510,9 @@ class XNode(QGraphicsItem):
         self.setAcceptHoverEvents(True)
         self.name = name
 
+        #i = GraphicsRectItem(-50, -50, 100, 100)
+        #self.parentItem().scene().addItem(i)
+
     def boundingRect(self):
         return QRectF(-25, -25, 50, 50)
 
@@ -262,9 +524,10 @@ class XNode(QGraphicsItem):
 
         p.setWidth(3)
         painter.setPen(p)
-        painter.drawRect(self.boundingRect())
+        #painter.GraphicsRectItem(self.boundingRect())
         p = QPen(Qt.green)
         painter.drawText(QPointF(-10, 0), self.name)
+
 
     def hoverEnterEvent(self, event):
         self.icolor = Qt.red
@@ -275,17 +538,8 @@ class XNode(QGraphicsItem):
     def hoverLeaveEvent(self, event):
         self.icolor = Qt.blue
         self.update()
-        #QApplication.instance().restoreOverrideCursor()
 
     def mouseMoveEvent(self, event):
-        #new_pos = event.scenePos()
-
-        # Keep the old Y position, so only the X-pos changes.
-        #old_pos = self.scenePos()
-        #new_pos.setY(old_pos.y())
-
-        #self.setPos(new_pos)
-
         global temporListNodes
         origCursorPos = event.lastScenePos()
         actualCursorPos = event.scenePos()
@@ -301,8 +555,7 @@ class XNode(QGraphicsItem):
         self.setPos(QPointF(aktualCursorPos_x, aktualCursorPos_y))
 
         print((aktualCursorPos_x, aktualCursorPos_y), "New position of x,y")
-    # We must override these or else the default implementation prevents
-    #  the mouseMoveEvent() override from working.
+
     def mousePressEvent(self, event):
         self.setSelected(True)
 
@@ -310,7 +563,6 @@ class XNode(QGraphicsItem):
 
     def addEdge(self, edge):
         self.edges.append(edge)
-
 
     def itemChange(self, change, value):
         if change == QGraphicsItem.ItemSelectedChange:
@@ -363,7 +615,6 @@ class Edge(QGraphicsLineItem):
     def adjust(self):
         self.prepareGeometryChange()
         self.setLine(QLineF(self.dest.pos() + QPointF(-10, -10), self.source.pos() + QPointF(-10, -10)))
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
